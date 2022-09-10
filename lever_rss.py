201a1586bdda9ba6ef3e67fdf6eadc32513cc498
TSharvananthan <thanussian@gmail.com>
2022-09-10 03:50:02 -0400

    Created _download_postings function.
    Given that I will aggregate the initial data in JSON, I can call a
    simple `r.get(url).json()`.
    
    For sakes of multithreading, I created a separate function that does
    the above.

--- lever_rss.py
+++ lever_rss.py
@@ -1,28 +1,33 @@
 from bs4 import BeautifulSoup
 import requests as r
 
+def _download_postings(name: str) -> list:
+    """Downloads postings for a company on lever.co
+
+    Arguments:
+        name -- The unique ID of the company
+
+    Returns:
+        A list of postings from a certain company
+    """
+    # Note that a companies job postings look like 
+    url_template = "https://api.lever.co/v0/postings/{}?mode=json"
+
+    url = url_template.format(name)
+    postings = r.get(url).json()
+
+    return postings
+
 def _job_xml_to_rss():
     pass
 
 def main():
-    # Note that a companies job postings look like 
-    url_template = "https://api.lever.co/v0/postings/{}?mode=json"
 
     # I got these names by doing the following
     #   1) Searched up "allinurl:jobs.lever.co"
     #   2) Copied every URL that started with "https://jobs.lever.co"
     #   3) Cleaned up the names and got the unique company IDs
     names = ["lever", "stackadapt", "koho", "taplytics", "homestars", "cohere", "atlassian", "invisible", "canva", "Medchart", "lever", "palantir", "wealthsimple", "benchsci", "gojek", "paytm", "yelp", "theathletic", "lime", "sfcg", "geotab", "waveapps", "nielsen", "newton", "arcteryx", "Polygon", "dave", "modsquad"]
-
-    for name in names:
-        with open(f"postings/{name}.xml", "w") as fp:
-            url = url_template.format(name)
-            c = r.get(url).content.decode("utf-8")
-
-            fp.write(c)
-
-        break
-
 
 if __name__ == "__main__":
     main()
