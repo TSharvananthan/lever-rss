33dd5dae34e8fcc4a7593d893d137b3c2e43367d
TSharvananthan <thanussian@gmail.com>
2022-09-10 14:16:33 -0400

    Finished implementation of RSS feeds
    I wish I commited a lot more during this process. However, I'm done
    this part of the program. I've done the following:
    
        1. Implemented async requests to improve processing speed
        2. Added a published date to every entry.

--- lever_rss.py
+++ lever_rss.py
@@ -1,25 +1,38 @@
-from bs4 import BeautifulSoup
-import requests as r
 from feedgen.feed import FeedGenerator
+from time import sleep
+from datetime import datetime, timezone
+import grequests
 
-def _job_xml_to_rss(name: str):
-    url = f"https://api.lever.co/v0/postings/{name}?mode=json"
+def _handle_exception(request, exception):
+    print(f"{request} failed with exception {exception}")
+
+def _job_xml_to_rss(name, url, response):
+    postings = response.json()
+
+    if not isinstance(postings, list):
+        return
 
     feed = FeedGenerator()
+    feed.id(name)
     feed.title(f'{name.title()} Job Postings')
     feed.author({'name':'Thanussian Sharvananthan','email':'thanussian@gmail.com'})
     feed.link(href=url, rel='alternate')
-
-    postings = postings = r.get(url).json()
+    feed.subtitle(f'Job postings for {name.title()} from lever.co')
+    feed.language('en')
 
     for posting in postings:
+        text = posting["text"]
+        url = posting["hostedUrl"]
+        description = posting["descriptionPlain"]
+        pub_date = datetime.fromtimestamp(posting["createdAt"] / 1000, tz=timezone.utc)
+
         new_entry = feed.add_entry()
-        new_entry.title(posting["text"])
-        new_entry.link(href=posting["hostedUrl"])
-        new_entry.description(posting["descriptionPlain"])
+        new_entry.title(text)
+        new_entry.link(href=url)
+        new_entry.description(description)
+        new_entry.published(pub_date)
 
-    feed.rss_file(f"{name.lower()}.rss.xml")
-
+    feed.rss_file(f"feeds/{name.lower()}.rss.xml")
 
 def main():
     # I got these names by doing the following
@@ -28,7 +41,17 @@ def main():
     #   3) Cleaned up the names and got the unique company IDs
     names = ["lever", "stackadapt", "koho", "taplytics", "homestars", "cohere", "atlassian", "invisible", "canva", "Medchart", "lever", "palantir", "wealthsimple", "benchsci", "gojek", "paytm", "yelp", "theathletic", "lime", "sfcg", "geotab", "waveapps", "nielsen", "newton", "arcteryx", "Polygon", "dave", "modsquad"]
 
-    print(_job_xml_to_rss("wealthsimple"))
+    urls = [f"https://api.lever.co/v0/postings/{name}?mode=json" for name in names]
+
+    all_requests = (grequests.get(url) for url in urls)
+
+    responses = grequests.imap(
+        all_requests,
+        exception_handler=_handle_exception
+    )
+
+    for name, url, response in zip(names, urls, responses):
+        _job_xml_to_rss(name, url, response)
 
 if __name__ == "__main__":
     main()
