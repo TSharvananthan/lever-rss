39c2456313e8a639fbb416c6c7f67b240656e741
TSharvananthan <thanussian@gmail.com>
2022-09-10 04:24:29 -0400

    Removed _download_postings function
    TURNS OUT... I DON'T NEED IT. Given that the URL is being used in
    _job_xml_to_rss, it's pretty redundant to include it.

--- lever_rss.py
+++ lever_rss.py
@@ -1,33 +1,34 @@
 from bs4 import BeautifulSoup
 import requests as r
+from feedgen.feed import FeedGenerator
 
-def _download_postings(name: str) -> list:
-    """Downloads postings for a company on lever.co
+def _job_xml_to_rss(name: str):
+    url = f"https://api.lever.co/v0/postings/{name}?mode=json"
 
-    Arguments:
-        name -- The unique ID of the company
+    feed = FeedGenerator()
+    feed.title(f'{name.title()} Job Postings')
+    feed.author({'name':'Thanussian Sharvananthan','email':'thanussian@gmail.com'})
+    feed.link(href=url, rel='alternate')
 
-    Returns:
-        A list of postings from a certain company
-    """
-    # Note that a companies job postings look like 
-    url_template = "https://api.lever.co/v0/postings/{}?mode=json"
+    postings = postings = r.get(url).json()
 
-    url = url_template.format(name)
-    postings = r.get(url).json()
+    for posting in postings:
+        new_entry = feed.add_entry()
+        new_entry.title(posting["text"])
+        new_entry.link(href=posting["hostedUrl"])
+        new_entry.description(posting["descriptionPlain"])
 
-    return postings
+    feed.rss_file(f"{name.lower()}.rss.xml")
 
-def _job_xml_to_rss():
-    pass
 
 def main():
-
     # I got these names by doing the following
     #   1) Searched up "allinurl:jobs.lever.co"
     #   2) Copied every URL that started with "https://jobs.lever.co"
     #   3) Cleaned up the names and got the unique company IDs
     names = ["lever", "stackadapt", "koho", "taplytics", "homestars", "cohere", "atlassian", "invisible", "canva", "Medchart", "lever", "palantir", "wealthsimple", "benchsci", "gojek", "paytm", "yelp", "theathletic", "lime", "sfcg", "geotab", "waveapps", "nielsen", "newton", "arcteryx", "Polygon", "dave", "modsquad"]
+
+    print(_job_xml_to_rss("wealthsimple"))
 
 if __name__ == "__main__":
     main()
